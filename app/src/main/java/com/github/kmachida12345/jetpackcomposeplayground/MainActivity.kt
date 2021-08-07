c69b105371b4163665f45064282fc7a28a95cbd5
kmachida12345 <kmachida12345@gmail.com>
2021-08-07 17:28:38 +0900

    codelab layouts-3

--- app/src/main/java/com/github/kmachida12345/jetpackcomposeplayground/MainActivity.kt
+++ app/src/main/java/com/github/kmachida12345/jetpackcomposeplayground/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.github.kmachida12345.jetpackcomposeplayground
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.github.kmachida12345.jetpackcomposeplayground.ui.theme.JetpackComposePlaygroundTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,17 +31,73 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackComposePlaygroundTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Column {
+                    Surface(color = MaterialTheme.colors.background) {
+                        Greeting("Android")
+                    }
+
+                    PhotographerCard()
+
                 }
             }
         }
     }
 }
 
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier
+            .padding(16.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colors.primary)
+
+            .clickable {
+                Log.d("machida", "PhotographerCard: hogehoge")
+            }
+            .padding(120.dp)
+
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+
+        }
+        Column(modifier = Modifier
+            .padding(start = 8.dp)
+            .align(Alignment.CenterVertically)
+        ) {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            // LocalContentAlpha is defining opacity level of its children
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    JetpackComposePlaygroundTheme {
+        PhotographerCard()
+    }
+}
+
+
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row {
+        Image(painter = painterResource(id = R.drawable.abc_vector_test), contentDescription = "test desu")
+        Column {
+            Text(text = "Hello $name!")
+            Text(text = "hogehoge!!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
